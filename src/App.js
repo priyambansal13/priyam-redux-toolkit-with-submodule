f7cdfb42ec30553808f6e981268e3133276029c3
priyambansal13 <bansalpriyam17@gmail.com>
2025-07-23 13:34:54 +0530

    test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { AddToDoComponent } from "./components/add-to-do.component";
 import { ToDosComponent } from "./components/to-dos.component";
+import { helloWorld } from "./reactjs/App.util";
 
 function App() {
+  const result = helloWorld();
+  console.info("Result from App.util.js:", result);
   return (
     <div className="App">
       <AddToDoComponent />
