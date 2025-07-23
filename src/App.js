bb2ed1934870e6d0917a87f8e556d6988fa8155e
priyambansal13 <bansalpriyam17@gmail.com>
2025-07-23 13:39:39 +0530

    used function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { AddToDoComponent } from "./components/add-to-do.component";
 import { ToDosComponent } from "./components/to-dos.component";
-import { helloWorld } from "./reactjs/App.util";
+import { greetUser, helloWorld } from "./reactjs/App.util";
 
 function App() {
   const result = helloWorld();
+  const name = greetUser("John Doe");
+  console.info("Greeting:", name);
   console.info("Result from App.util.js:", result);
   return (
     <div className="App">
